dc9a8d0878c7285b233f2097806e984834d65fca
codemaster-stack <tegaabinuwa@gmail.com>
2026-03-28 08:02:43 +0100

    cors

--- app.js
+++ app.js
@@ -26,14 +26,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 // Rate limiting
 const limiter = rateLimit({
